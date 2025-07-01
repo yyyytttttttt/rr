03a139a64b663199e92bd2ea0bbff798a35441d7
Владислав Соголаев <yaroslavsogolaev@yandex.ru>
2025-07-02 00:37:11 +0300

    some message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
 
-export default nextConfig;
+  experimental: {
+    serverActions: {}, // ⬅️ теперь без ошибки
+  },
+
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
