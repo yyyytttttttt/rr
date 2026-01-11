95e6a297f8c42d455fd998354f5fd4854dfe14cd
Владислав Соголаев <yaroslavsogolaev@yandex.ru>
2026-01-11 21:01:17 +0300

    sysysysysy

--- src/app/api/debug/doctor/[doctorId]/route.ts
+++ src/app/api/debug/doctor/[doctorId]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "../../../../../../lib/prizma";
+import { prisma } from "../../../../../lib/prizma";
 
 /**
  * GET /api/debug/doctor/[doctorId]
